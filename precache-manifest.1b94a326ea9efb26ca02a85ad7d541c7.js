e87c4ab726a05c4df54857d21c192d2e63ec34fc
nickb <nickb@twintels-MBP.lan>
2020-08-11 11:52:01 -0700

    Updates

---
+++ precache-manifest.1b94a326ea9efb26ca02a85ad7d541c7.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0872a81fa161c5980a634da722f2954",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "626d9332dda1bad3732e",
+    "url": "/menu/static/js/2.ed8ca7ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/menu/static/js/2.ed8ca7ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea289b1f700f99003aa",
+    "url": "/menu/static/js/main.706c6bd3.chunk.js"
+  },
+  {
+    "revision": "c93c4944bde666294912",
+    "url": "/menu/static/js/runtime-main.cf8b6d9c.js"
+  },
+  {
+    "revision": "0ab1edcbcc86c33b1c8fdf4b4e1acb8b",
+    "url": "/menu/static/media/3-dot.0ab1edcb.svg"
+  },
+  {
+    "revision": "5f6b5d6d816b7516abe92d481903da90",
+    "url": "/menu/static/media/Bell.5f6b5d6d.svg"
+  },
+  {
+    "revision": "a29b3a73f6f6932d14029d1e8b31252a",
+    "url": "/menu/static/media/Info -  Tool tip.a29b3a73.svg"
+  },
+  {
+    "revision": "80d8fcc03339b44f32b7adc14b9dc537",
+    "url": "/menu/static/media/Phone.80d8fcc0.svg"
+  },
+  {
+    "revision": "98059568a8242c9423f62f66734357ce",
+    "url": "/menu/static/media/Search.98059568.svg"
+  },
+  {
+    "revision": "9924b53983909560cbbe227fd09c0ba2",
+    "url": "/menu/static/media/Star.9924b539.svg"
+  },
+  {
+    "revision": "9723f724af5a129ff4eba83020abc35d",
+    "url": "/menu/static/media/account.9723f724.svg"
+  },
+  {
+    "revision": "1a561b8edd5e565f69f35c91febde066",
+    "url": "/menu/static/media/action.1a561b8e.svg"
+  },
+  {
+    "revision": "75aaf3ef9f395360bc290365a258dcf4",
+    "url": "/menu/static/media/activity.75aaf3ef.svg"
+  },
+  {
+    "revision": "052e8f71bedf5b53e7ab9eeca953c7c4",
+    "url": "/menu/static/media/burgNav.052e8f71.svg"
+  },
+  {
+    "revision": "596acdd04c38521efd3e9633dc84687f",
+    "url": "/menu/static/media/comps.596acdd0.svg"
+  },
+  {
+    "revision": "258e63d75b5cd08fd09238eb8cba6c05",
+    "url": "/menu/static/media/contacts.258e63d7.svg"
+  },
+  {
+    "revision": "a2296a31ddf51c4b4b6399412b1bb37a",
+    "url": "/menu/static/media/dashboard.a2296a31.svg"
+  },
+  {
+    "revision": "2d861e3b8d02e2c6534f5e2e3d5f3cb8",
+    "url": "/menu/static/media/doubleArrow.2d861e3b.svg"
+  },
+  {
+    "revision": "a62a2fe9d08a887d72187e295a0e40b0",
+    "url": "/menu/static/media/e-campaigns.a62a2fe9.svg"
+  },
+  {
+    "revision": "b84d14651a158b8eac199fe4ba0efe59",
+    "url": "/menu/static/media/email-lists.b84d1465.svg"
+  },
+  {
+    "revision": "381d287613cc5d76eb9b92ff62da94f3",
+    "url": "/menu/static/media/filter.381d2876.svg"
+  },
+  {
+    "revision": "1295118ac20d68e880599224317af537",
+    "url": "/menu/static/media/listings.1295118a.svg"
+  },
+  {
+    "revision": "10836d3604134819ac541a0b775fcca0",
+    "url": "/menu/static/media/message-filled.10836d36.svg"
+  },
+  {
+    "revision": "c80d5219b32168105573cf65d980ae7c",
+    "url": "/menu/static/media/messages.c80d5219.svg"
+  },
+  {
+    "revision": "6a9d77c1fbb05d3e937b5385b9901cfc",
+    "url": "/menu/static/media/notification-settings.6a9d77c1.svg"
+  },
+  {
+    "revision": "7f1a804b6052a32c53b1a109590f4f93",
+    "url": "/menu/static/media/profile.7f1a804b.svg"
+  },
+  {
+    "revision": "b3bdf911174c39850c37bc8c53c855bc",
+    "url": "/menu/static/media/pursuits.b3bdf911.svg"
+  },
+  {
+    "revision": "41768f9b14a3b31a13c0a5060ab8543f",
+    "url": "/menu/static/media/saved-search.41768f9b.svg"
+  },
+  {
+    "revision": "2a515a4ccb747550b7579bd6a63e8835",
+    "url": "/menu/static/media/saved.2a515a4c.svg"
+  },
+  {
+    "revision": "327af698ca53ea7d67cb838d18a7e2bc",
+    "url": "/menu/static/media/tracker.327af698.svg"
+  }
+]);
